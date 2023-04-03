91cf635d7948bafe414a02a9f625d232bb2f91ed
Enes kadir  Dönmez <>
2023-04-04 00:39:36 +0300

    deneme3

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,12 @@ import 'package:myapp/page-1/all-brands.dart';
 // import 'package:myapp/page-1/onboarding3.dart';
 // import 'package:myapp/page-1/onboardin2.dart';
 // import 'package:myapp/page-1/onboarding1.dart';
-// import 'package:myapp/page-1/bilgisayar-donanm-malzemelerinin-satn-yapmak-istediimiz-bir-mobil-uygulamadrkendimize-ait-olan-marka-admz-resimlerimiz-ve-rnlerimizi-kullanarak-uygulamay-indiren-insanlara-rnlerimizi-satmay-amalayan-bir-uygulamadr.dart';
+// import 'package:myapp/page-1/bilgisayar-donanm-malzemelerinin-satn-yapmak-istediimi-bir-mobil-uygulamadrkendimize-ait-olan-marka-admz-resimlerimiz-ve-rnlerimizi-kullanarak-uygulamay-indiren-insanlara-rnlerimizi-satmay-amalayan-bir-uygulamadr.dart';
 // import 'package:myapp/page-1/homescreen.dart';
 // import 'package:myapp/page-1/sign-in.dart';
 
 void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
 	@override
